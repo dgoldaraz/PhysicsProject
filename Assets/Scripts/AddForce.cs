2a10a0f9232b50f897267842467b2b323f314454
dgoldaraz <dgoldaraz@gmail.com>
2016-08-02 20:05:32 +0100

    Changes on the engine

---
+++ Assets/Scripts/AddForce.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PhysicsEngine))]
+public class AddForce : MonoBehaviour {
+
+    public Vector3 force;
+    private PhysicsEngine physics;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        physics = GetComponent<PhysicsEngine>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        physics.AddForce(force);
+	}
+}
--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -7,47 +7,97 @@ public class PhysicsEngine : MonoBehaviour {
 
     public Vector3 velocityVector; //average velocity call in FixdUpdate
 
-    public Vector3 netForces;
-    public List<Vector3> forceVectorList = new List<Vector3>();
+    public Vector3 netForce;
+    private List<Vector3> forceVectorList = new List<Vector3>();
     public float mass = 1.0f;
-	// Use this for initialization
-	void Start () {
+
+
+    public bool showTrails = true;
+    private LineRenderer lineRenderer;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        InitDrawingTrails();
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void InitDrawingTrails()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.SetColors(Color.yellow, Color.yellow);
+        lineRenderer.SetWidth(0.2F, 0.2F);
+        lineRenderer.useWorldSpace = false;
+    }
 
     /// <summary>
     /// This is call every 20 ms 
     /// </summary>
     void FixedUpdate()
     {
-        AddForces();
-        //First Newtonlaw, move constantly if the net force is equal to 0
+        DrawTrails();
+        UpdatePosition();
+    }
+
+    /// <summary>
+    /// Adds forces to the list
+    /// </summary>
+    /// <param name="newForce"></param>
+    public void AddForce(Vector3 newForce)
+    {
+        forceVectorList.Add(newForce);
+    }
+
+    /// <summary>
+    /// Updates the position dependant on the acceleration (forces) and velocity
+    /// </summary>
+    void UpdatePosition()
+    {
+        //First Newton law, if forces is 0, the change of velocity is 0
         //Second Netwon Law, F = ma => a = F/m
-        UpdateVelocity();
-        //Update position
-        transform.position += velocityVector * Time.deltaTime;
-        
-    }
+        //Third Law - There is an equal force in the inverse direction when an object hit another
 
-    void AddForces()
-    {
-        netForces = Vector3.zero;
-        foreach(Vector3 f in forceVectorList)
+        //SUm forces
+        netForce = Vector3.zero;
+        foreach (Vector3 f in forceVectorList)
         {
-            netForces += f;
+            netForce += f;
         }
-    }
 
-    void UpdateVelocity()
-    {
+        forceVectorList.Clear();
+
         //Change the velocity dependant of the forces;
         //Calculate acceleration 
-        Vector3 acceleration = netForces / mass;
+        Vector3 acceleration = netForce / mass;
         velocityVector += acceleration * Time.deltaTime;
+
+        //Update position
+        transform.position += velocityVector * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Draw the forces applied if it's neccesary
+    /// </summary>
+    void DrawTrails()
+    {
+        if (showTrails)
+        {
+            lineRenderer.enabled = true;
+            int numberOfForces = forceVectorList.Count;
+            lineRenderer.SetVertexCount(numberOfForces * 2);
+            int i = 0;
+            foreach (Vector3 forceVector in forceVectorList)
+            {
+                lineRenderer.SetPosition(i, Vector3.zero);
+                lineRenderer.SetPosition(i + 1, -forceVector);
+                i = i + 2;
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
     }
 }
