e2aa336f35ea995688bf16139027f374a3c0a054
dgoldaraz <dgoldaraz@gmail.com>
2016-09-12 09:13:02 +0100

    Scenes and Wind
    Add 2 new scenes
    Polish old ones
    Add new gravity scene
    Add new Wind Prefab

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+    public Vector3 windDirection;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Rocket"))
+        {
+            AddForce addForceComponent = col.GetComponent<AddForce>();
+            if(addForceComponent)
+            {
+                Vector3 cForce = addForceComponent.force;
+                cForce += windDirection;
+                addForceComponent.force = cForce;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject.CompareTag("Rocket"))
+        {
+            AddForce addForceComponent = col.GetComponent<AddForce>();
+            if (addForceComponent)
+            {
+                Vector3 cForce = addForceComponent.force;
+                cForce -= windDirection;
+                addForceComponent.force = cForce;
+            }
+        }
+    }
+}
